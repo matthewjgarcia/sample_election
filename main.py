1e0f1d72be069c5abc9ed1cafa6c13f5b42e580b
matthewjgarcia <35940299+matthewjgarcia@users.noreply.github.com>
2023-07-21 17:22:31 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+survey_responses = ['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos']
+total_ceballos =(sum([1 for i in survey_responses if i == 'Ceballos']))
+print(total_ceballos)
+survey_length =float(len(survey_responses))
+print(survey_length)
+percentage_ceballos = (total_ceballos / survey_length)
+print(percentage_ceballos)
+
+possible_surveys = np.random.binomial(survey_length, .54, size = 10000) / survey_length
+
+plt.hist(possible_surveys, range = (0, 1), bins=20)
+plt.show()
+
+ceballos_loss_surveys = np.mean(possible_surveys < .5)
